3cfcf897b98b9e37ff0878d90847b311447b86fe
the Phoeron <thephoeron@users.noreply.github.com>
2015-05-12 10:03:40 -0400

    Update arithmetic.lisp
    update copyright

--- arithmetic.lisp
+++ arithmetic.lisp
@@ -1,7 +1,7 @@
 ;;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: BIT-SMASHER; Base: 10 -*-
 ;;;; arithmetic.lisp
 
-;;;; Copyright (c) 2014 "the Phoeron" Colin J.E. Lupton <//thephoeron.com>
+;;;; Copyright (c) 2014--2015, "the Phoeron" Colin J.E. Lupton <//thephoeron.com>
 ;;;; See LICENSE for additional information.
 
 (in-package :bit-smasher)
