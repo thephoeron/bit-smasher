c61d044e482b0a79f8742b86a6ad34a1d118fe47
the Phoeron <thephoeron@users.noreply.github.com>
2015-06-06 00:08:19 -0400

    Update bit-smasher.lisp

--- t/bit-smasher.lisp
+++ t/bit-smasher.lisp
@@ -2,4 +2,20 @@
 
 (in-package :cl-user)
 
+(defpackage bit-smasher-test
+  (:use cl bit-smasher prove))
+
+(in-package :bit-smasher-test)
+
+;; NOTE: To run this test file, execute `(asdf:test-system :bit-smasher)' in your Lisp.
+
+(plan 1)
+
+(deftest sanity-check
+  (is (+ 1 1)
+      2
+      "Sane Lisp system."))
+
+(run-test-all)
+
 ;; EOF
