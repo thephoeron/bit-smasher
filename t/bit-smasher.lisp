0594accc2a4dd2bff4093de92b5df08670bd157c
the Phoeron <sysop@thephoeron.com>
2015-06-27 00:50:12 -0400

    add conversion test

--- t/bit-smasher.lisp
+++ t/bit-smasher.lisp
@@ -9,7 +9,7 @@
 
 ;; NOTE: To run this test file, execute `(asdf:test-system :bit-smasher)' in your Lisp.
 
-(plan 2)
+(plan 3)
 
 (deftest sanity-check
   (pass "PROVE is loaded and ready to go.")
@@ -21,6 +21,16 @@
   (is (* 2 2)
       4
       "Multiplication: (* 2 2) => 4."))
+
+;; Conversion test
+
+(deftest conversion
+  (is (hex<- (expt 10 64))
+      "184f03e93ff9f4daa797ed6e38ed64bf6a1f010000000000000000"
+      "Integer to Hex-String conversion.")
+  (is (bits<- (expt 10 64))
+      #*000110000100111100000011111010010011111111111001111101001101101010100111100101111110110101101110001110001110110101100100101111110110101000011111000000010000000000000000000000000000000000000000000000000000000000000000
+      "Integer to Bit-Vector conversion."))
 
 ;; Two's Complement Test
 
